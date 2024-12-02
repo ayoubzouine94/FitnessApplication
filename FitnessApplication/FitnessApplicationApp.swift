4660281084cd50478061604139cb4c24aaa75fcb
Ayoub Zouine <ayoubzouine94@gmail.com>
2024-12-02 10:03:56 +0100

    Initial Commit

---
+++ FitnessApplication/FitnessApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessApplicationApp.swift
+//  FitnessApplication
+//
+//  Created by MAC on 2/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct FitnessApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
