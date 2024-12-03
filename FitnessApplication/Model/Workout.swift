ed02976d48eb8e0f995930b6c59dbc9e8ba8f9b2
Ayoubzouine94 <ayoubzouine94@gmail.com>
2024-12-03 16:13:51 +0100

    Separate Models

---
+++ FitnessApplication/Model/Workout.swift
@@ -0,0 +1,18 @@
+//
+//  Workout.swift
+//  FitnessApplication
+//
+//  Created by MAC on 3/12/2024.
+//
+
+import SwiftUI
+
+struct Workout: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Workout()
+}
